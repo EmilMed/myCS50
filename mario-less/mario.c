f8fc1e62bd5a46123cabfc93bc5c3e8798f1c10a
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-11 11:17:25 +0000

    Tue, Jul 11, 2023, 12:17 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,7 +11,7 @@ int main(void)
     while (n > 8 || n < 1);
 // For EACH ROW
     for (int g = 0; g < n; g++)
-     {
+     { // For EACH COLUMN
         for (int h = 0; h < n; h++)
           { if (g + h > n - 2)
               printf("#");
