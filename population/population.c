3292a53aa8251def5cb797364cda0eed21257f22
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-11 20:33:07 +0000

    Tue, Jul 11, 2023, 9:33 PM +01:00

--- population/population.c
+++ population/population.c
@@ -4,7 +4,11 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
+do
+  {
+      n = get_int("Starting population size ");
+  }
+  while (n > 9);
     // TODO: Prompt for end size
 
     // TODO: Calculate number of years until we reach threshold
