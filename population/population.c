b3e0a05bfe5741d389bbf8601774a887fbedd223
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-13 20:10:18 +0000

    Thu, Jul 13, 2023, 9:10 PM +01:00

--- population/population.c
+++ population/population.c
@@ -7,7 +7,7 @@ int main(void)
     int n;
 do
   {
-      n = get_int("Start size ");
+      n = get_int("Start size:20 ");
   }
   while (n < 9);
     // TODO: Prompt for end size
@@ -19,7 +19,7 @@ do
   while (x < n);
     // TODO: Calculate number of years until we reach threshold
      int y;
-    y = (x - n) / ((n / 3) - (n / 4))
+    y = (x - n) / ((n / 3) - (n / 4));
     // TODO: Print number of years
     printf("Years: %i\n", y);
 }
