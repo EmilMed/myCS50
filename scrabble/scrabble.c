442905d2ad3a8a786653668f1063bce2eeffa76c
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-16 12:11:32 +0000

    Sun, Jul 16, 2023, 1:11 PM +01:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -36,6 +36,7 @@ int main(void)
 int compute_score(string word)
 {
     int score = 0;
+    
     // TODO: Compute and return score for string
     for (int i = 0; len = strlen(word); i < len; i++)
     {
