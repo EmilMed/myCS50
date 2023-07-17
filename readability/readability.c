90000b94553f0bf1a722027944f0a266916044ab
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-17 12:54:48 +0000

    Mon, Jul 17, 2023, 1:54 PM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,9 +8,11 @@ int main(void)
 {
     string text = get_string("Text: ");
 
-    int letters = 0
+    int letters = 0;
     for (int i = 0; i < strlen(text); i++)
     {
-        if((text[i]))
+        if(islower(text[i])) || (isupper(text[i]))
+        letters++;
     }
+    printf("%i letters\n", i)
 }
