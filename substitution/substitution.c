856455c426e15d03b2bb5087cdc3649c3884d673
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-17 17:53:59 +0000

    Mon, Jul 17, 2023, 6:53 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -7,9 +7,16 @@ int main(int argc, string argv[])
 {
   if (argc != 2)
   {
-    printf("Usage: ./substitution key");
+    printf("Usage: ./substitution key\n");
     return 1;
   }
-  else if ()
+  string key = argv[1];
+  for(i = 0; i < strlen(key); i++)
+  {
+    if (!isalpha(key[i]))
+    printf("Usage: ./substitution key\n");
+    return 1;
+  }
+  if argv[]
 
 }
