5b4a2f8135ae587d5497617ed04739666564ee99
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-20 18:38:21 +0000

    Thu, Jul 20, 2023, 7:38 PM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,10 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    if(name = )
+    if(name = argv[i])
+    {
+        
+    }
     return false;
 }
 
