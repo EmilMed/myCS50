45b1b832a04510f78ad0c9cae8d2cc7fd78327ac
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-22 15:37:20 +0000

    Sat, Jul 22, 2023, 4:37 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -59,6 +59,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height + 1; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
     return;
 }
 
