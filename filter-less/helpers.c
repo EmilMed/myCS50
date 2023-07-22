90313d39d9a0e29e5985ccb62d3fd353199c0f65
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-22 15:17:52 +0000

    Sat, Jul 22, 2023, 4:17 PM +01:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -24,13 +24,35 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    or (int i = 0; i < height + 1; i++)
+    for (int i = 0; i < height + 1; i++)
     {
         for (int j = 0; j < width + 1; j++)
         {
-    sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
-    sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
-    sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+            float originalRed = image[i][j].rgbtRed;
+            float originalBlue = image[i][j].rgbtBlue;
+            float originalGreen = image[i][j].rgbtGreen;
+
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtGreen = sepiaGreen;
+        }
+    }
     return;
 }
 
