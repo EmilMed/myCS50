80e3f0fc145085d50d5814672673681c6647d409
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-23 14:44:36 +0000

    Sun, Jul 23, 2023, 3:44 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 
  for(i = 0; i < memory card length; i++)
  {
-    fread(&buffer, 512 , 1, card_raw)
+    while (fread(buffer, 512 , 1, f))
     if(buffer[0] = 0xff && buffer[1] = 0xd8 && buffer[2] = 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
         sprintf
