4d5211cae3002584032a08c115a18532e5a8e589
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-23 10:52:08 +0000

    Sun, Jul 23, 2023, 11:52 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,25 +5,23 @@ int main(int argc, char *argv[])
 {
  if(argc != 3)
  {
-    
+
 
  }
  FILE *f = fopen(card_raw, "r")
 
 int JPEG_counter = 0;
-
-fread(&buffer, 512 , 1, card_raw)
-if(buffer[0] = 0xff && buffer[1] = 0xd8 && buffer[2] = 0xff && (buffer[3] & 0xf0) == 0xe0)
-{
-    return true;
-}
-
  for(i = 0; i < memory card length; i++)
  {
-    if(block has oxff, oxd8, oxff and either of any forth ones)
+    fread(&buffer, 512 , 1, card_raw)
+    if(buffer[0] = 0xff && buffer[1] = 0xd8 && buffer[2] = 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
-
-        JPEG_counter++;
+        JPEG_counter++
+        if (JPEG_counter = 1)
+        {
+            
+        }
     }
  }
 }
+
