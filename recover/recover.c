6533fdfc59d88dfe62fb5757ea87c0dcf98cdbb7
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-23 10:46:06 +0000

    Sun, Jul 23, 2023, 11:46 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -12,7 +12,10 @@ int main(int argc, char *argv[])
 int JPEG_counter = 0;
 
 fread(&buffer, 512 , 1, card_raw)
-if(buffer[0] = oxff && buffer[1] = oxd8 && buffer[2] = oxff && )
+if(buffer[0] = 0xff && buffer[1] = 0xd8 && buffer[2] = 0xff && (buffer[3] & 0xf0) == 0xe0)
+{
+    return true;
+}
 
  for(i = 0; i < memory card length; i++)
  {
