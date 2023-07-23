a4044bb75266e44bb54553f26ce21b24da528bbc
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-23 10:36:31 +0000

    Sun, Jul 23, 2023, 11:36 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,4 +8,13 @@ int main(int argc, char *argv[])
 
  }
  FILE *f = fopen(card_raw, "r")
+
+int JPEG_counter = 0;
+ for(i = 0; i < memory card length; i++)
+ {
+    if(block has oxff, oxd8, oxff and either of any forth ones)
+    {
+        JPEG_counter++;
+    }
+ }
 }
