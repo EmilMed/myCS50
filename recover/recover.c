193e678e93d5b03554b66882fc9ccd7ae7ca6cb3
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-23 10:54:06 +0000

    Sun, Jul 23, 2023, 11:54 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,7 +6,6 @@ int main(int argc, char *argv[])
  if(argc != 3)
  {
 
-
  }
  FILE *f = fopen(card_raw, "r")
 
@@ -16,10 +15,11 @@ int JPEG_counter = 0;
     fread(&buffer, 512 , 1, card_raw)
     if(buffer[0] = 0xff && buffer[1] = 0xd8 && buffer[2] = 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
+        sprintf
         JPEG_counter++
         if (JPEG_counter = 1)
         {
-            
+
         }
     }
  }
