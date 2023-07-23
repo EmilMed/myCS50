2d772b8b29b41d2f6bdcdc85807198fc2fcf07f2
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-23 15:14:44 +0000

    Sun, Jul 23, 2023, 4:14 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -40,11 +40,12 @@ int main(int argc, char *argv[])
       if (outputf != NULL)
         {
           fwrite(buffer, sizeof(char), 512, outputf);
+          if (JPEG_counter > 0)
+            {
+              fclose(outputf);
+            }
         }
-      if (JPEG_counter > 0)
-      {
-        fclose(outputf);
-      }
+
     }
 
     free(filename);
