e140489651af91ddecddad0d19886c915d046627
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-27 12:05:39 +0000

    Thu, Jul 27, 2023, 1:05 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -99,6 +99,20 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
+    for(int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor)
+        {
+            node *tmp = cursor;
+            cursor = cursor->next;
+            tmp = cursor;
+            free(tmp);
+        }
+        if (cursor == NULL && i )
+        {
+            return true;
+        }
+    }
     return false;
 }
