ecccd3ba8f0c18fb17e93b3aa12e108b782ad035
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-29 15:30:16 +0000

    Sat, Jul 29, 2023, 4:30 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -5,10 +5,10 @@ def main():
   x = get_height()
   for i in range(x):
     for j in range(x):
-      if (i + j > x - 2):
-        print("#")
-      else:
+      if (i + j > x - 1):
         print(" ", end="")
+      else:
+        print("#", end="")
   print()
 
 def get_height():
