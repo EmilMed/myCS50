7732d742b7832919d85b4647da0033501901bff5
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-29 14:58:21 +0000

    Sat, Jul 29, 2023, 3:58 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,7 +1,14 @@
 import cs50 import get_int
 
 def main():
-
+  height = get_height()
+  for i in range(height):
+    for j in range(height):
+      if i + j > n - 2:
+        print("#")
+      else
+        print(" ")
+   
 
 
 def get_height():
