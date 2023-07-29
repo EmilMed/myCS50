e95106c3654c46dcc69c15aab6d218406abf10ad
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-29 15:12:24 +0000

    Sat, Jul 29, 2023, 4:12 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,9 +3,8 @@ from cs50 import get_int
 def get_height():
    while True:
     n = get_int("Height: ")
-    if n > 8 and n < 1:
-      break
-   return n
+    if n < 9 and n > 0:
+      return n
 
 def main():
   height = get_height()
