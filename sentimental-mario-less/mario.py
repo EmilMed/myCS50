7a0ed94768ceacafa4cb6a2df83714f2c939fcb6
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-29 14:51:32 +0000

    Sat, Jul 29, 2023, 3:51 PM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,9 +2,12 @@ import cs50 import get_int
 
 while True:
     n = get_int("Height: ")
-    if n > 8 or n < 1;
+    if n > 8 or n < 1:
         return False
     else
         return n
+for i in row:
+    for j in column:
+
 
 
