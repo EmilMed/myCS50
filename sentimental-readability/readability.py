19d58ce88c96a9c1eade0605199f8a82a83d86a1
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-31 10:19:55 +0000

    Mon, Jul 31, 2023, 11:19 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,11 +1,9 @@
 from cs50 import get_string
 
 text = get_string("Text: ")
-
 letters = 0
 words = 1
 sentences = 0
-
 for i in text:
     if i.isalpha():
         letters += 1
@@ -13,9 +11,7 @@ for i in text:
         words += 1
     elif i == "." or i == "!" or i == "?":
         sentences += 1
-
-index = 0.0588 * (letters/words * 100) - 0.296 * (sentences/words * 100) - 15.8
-
+index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 if index < 1:
     print("Before Grade 1")
 elif index > 16:
