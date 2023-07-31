c1422591d68041dcd1edd358751265a480d2a0ac
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-31 16:18:26 +0000

    Mon, Jul 31, 2023, 5:18 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -4,33 +4,35 @@ import sys
 
 def main():
 
-    # TODO: Check for command-line usage
     if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
         sys.exit(1)
 
-    # TODO: Read database file into a variable
     database = []
     with open(sys.argv[1], 'r') as file:
       reader = csv.DictReader(file)
           for row in reader:
               database.append(row)
 
-    # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2], 'r') as file:
       dna_sequence = file.read()
 
-    # TODO: Find longest match of each STR in DNA sequence
     subsequences = list(database[0].keys())[1:]
 
     finals = {}
     for subsequence in subsequences:
         final[subsequence] = longest_match(dna_sequence, subsequence)
 
+    for individual in database:
+        bingo = 0
+        for subsequence in subsequences:
+            if int(individual[subsequence]) == final[subsequence]:
+                bingo += 1
+        if bingo == len(subsequences):
+            print(individual["name"])
+            return
 
-    # TODO: Check database for matching profiles
-
-    return
+    print("No match.")
 
 
 def longest_match(sequence, subsequence):
