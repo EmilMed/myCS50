93bafccc1d36d027e7912e835be76e99b68cdb57
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-31 15:19:10 +0000

    Mon, Jul 31, 2023, 4:19 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,9 +5,13 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if sys.
+    if len(sys.argv) != 3:
+        print("Usage: python dna.py data.csv sequence.txt")
+        sys.exit(1)
 
     # TODO: Read database file into a variable
+
+    
 
     # TODO: Read DNA sequence file into a variable
 
