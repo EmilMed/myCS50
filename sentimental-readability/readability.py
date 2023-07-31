0ab87269970f2ae3e70d03bc9c8eaf022aa49fe2
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-07-31 10:13:11 +0000

    Mon, Jul 31, 2023, 11:13 AM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,6 +1,4 @@
 from cs50 import get_string
-from math import round
-from stdio import isalpha
 
 text = get_string("Text: ")
 
@@ -9,9 +7,11 @@ words = 1
 sentences = 0
 
 for i in range(len(text)):
-    if isalpha(text[i]):
+    if i.isalpha():
         letters += 1
-    elif text[i] == " ":
+    elif i == " ":
         words += 1
-    elif text[i] == "." or text[i] == "!" or text[i] == "?":
+    elif i == "." or i == "!" or i == "?":
         sentences += 1
+
+
