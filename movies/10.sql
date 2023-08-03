6dbfd6947949f14a9c1044a0e7fc8a3650e409f7
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-03 17:14:31 +0000

    Thu, Aug 3, 2023, 6:14 PM +01:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT name FROM people JOIN ratings on 
+SELECT DISTINCT name FROM people JOIN directors on directors.person_id = people.id JOIN movies ON movies.id = directors.movie_id JOIN ratings ON ratings.movie_id = movies.id WHERE rating >= 9.0;
