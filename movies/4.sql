7fcd8a4617a1601585dacb74849b6d038389a2e5
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-03 13:58:53 +0000

    Thu, Aug 3, 2023, 2:58 PM +01:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE movie_id = (SELECT id FROM ratings WHERE rating = 10.0);
+SELECT COUNT(rating) FROM movies WHERE movie_id = (SELECT id FROM ratings WHERE rating = 10.0);
