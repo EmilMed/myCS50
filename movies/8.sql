409592b586c2e77e0c4fde6d9a366c25dbbe804f
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-03 16:57:20 +0000

    Thu, Aug 3, 2023, 5:57 PM +01:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON movies_id = stars.movie_id WHERE movies.title = "Toy Story";
+SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON movies.id = stars.movie_id WHERE movies.title = "Toy Story";
