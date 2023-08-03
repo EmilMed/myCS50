6d51ef1bc906bce120361a470f1dd6bf51d433b6
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-03 17:45:01 +0000

    Thu, Aug 3, 2023, 6:45 PM +01:00

---
+++ movies/13.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT name FROM people JOIN movies ON stars.movie_id = movies.id JOIN stars ON stars.person_id = people.id WHERE movies.id IN (SELECT movies.id FROM movies JOIN stars ON stars.movie_id = movies.id JOIN people ON people.id = stars.person_id WHERE people.name = "Kevin Bacon" AND people.birth = 1958);
