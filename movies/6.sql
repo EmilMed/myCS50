12f86cae4ee7996c587dc0d66f50946681b77b8e
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-03 14:16:54 +0000

    Thu, Aug 3, 2023, 3:16 PM +01:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,1 +1,1 @@
-SELECT AVG(rating) FROM movies JOIN ratings on movies.id = ratings.movies_id WHERE year = 2012;
+SELECT AVG(rating) FROM movies JOIN ratings on movies.id = ratings.movie_id WHERE year = 2012;
