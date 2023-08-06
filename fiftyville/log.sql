c5c7e35e3f06560b6c89ac224e3e9c7fe96ee7db
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-06 11:06:36 +0000

    Sun, Aug 6, 2023, 12:06 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT description FROM crime_scene_reports
-WHERE year = 2021
-WHERE month = "July"
-WHERE day = 21
-WHERE street = "Humphrey street"
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = "July" AND day = 21;
