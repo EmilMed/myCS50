76123b3c049a568e47d5834dc9834f7bb70298a8
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-06 11:01:17 +0000

    Sun, Aug 6, 2023, 12:01 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 SELECT description FROM crime_scene_reports
-WHERE year = 
+WHERE year = 2021
+WHERE month = "July"
+WHERE day = 21
