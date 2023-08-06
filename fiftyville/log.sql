664d1b9200e559a3feee8376d25d50f6910672a0
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-06 13:08:37 +0000

    Sun, Aug 6, 2023, 2:08 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,4 +4,4 @@ SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND 
 
 SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 
-SELECT id, name, license_plate FROM poeple JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
+SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name FROM poeple JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
