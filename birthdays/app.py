cc98de9d74581b00f01c4fc8299eec7388c79355
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-20 15:20:58 +0000

    Sun, Aug 20, 2023, 4:20 PM +01:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -16,7 +16,7 @@ db = SQL("sqlite:///birthdays.db")
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
