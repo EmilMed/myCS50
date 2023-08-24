1cdaa7dc498c6f3310ebba7fb0d89b237f665fa8
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-24 10:22:07 +0000

    Thu, Aug 24, 2023, 11:22 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -108,9 +108,10 @@ def quote():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    passwords = db.execute("INSERT INTO users (username, password) VALUES (?,?)", username, password)
-    if not username or password not in password or confirmation not in passwords:
+    passwords = db.execute("SELECT * FROM passwords)
+    if not username or password not in passwords or confirmation not in passwords:
         return apology("apology.html")
+    db.execute("INSERT INTO users (username, password) VALUES (?,?)", username, password)
     return render_template("layout.html")
 
 
