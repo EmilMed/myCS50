b8bba887595540952feb58c5c19f6ff7bd675945
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-26 14:59:10 +0000

    Sat, Aug 26, 2023, 3:59 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -163,5 +163,5 @@ def register():
 def sell():
     if request.method == "GET":
         user_id = session["user_id"]
-        symbols = db.execute("SELECT symbol FROM cashflow WHERE user_id = :id HAVING SUM(shares) > 0 GROUP BY symbol" id=user_id)
-        return render_template("sell.html", symbols = [row["symbol"] for row in rows])
+        symbols_user = db.execute("SELECT symbol FROM cashflow WHERE user_id = :id GROUP BY symbol HAVING SUM(shares) > 0", id=user_id)
+        return render_template("sell.html", symbols = [row["symbol"] for row in symbols_user])
