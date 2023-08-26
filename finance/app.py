355504a5d42a1ed73c310ec16811717738b5028b
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-26 14:10:00 +0000

    Sat, Aug 26, 2023, 3:10 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -37,7 +37,7 @@ def index():
     user_id = session["user_id"]
     cashflow_db = db.execute("SELECT symbol, SUM(shares) AS shares, price FROM cashflow WHERE user_id = ? GROUP BY symbol", user_id)
     cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
-    cash = cash_db[0]["cash"]
+    cash = round(float(cash_db[0]["cash"]), 2)
     return render_template("index.html", database = cashflow_db, cash = cash)
 
 @app.route("/buy", methods=["GET", "POST"])
