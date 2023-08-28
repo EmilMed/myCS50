519907f94f7effc4c43a39a206775b2698fb4f50
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-28 12:25:53 +0000

    Mon, Aug 28, 2023, 1:25 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -136,7 +136,7 @@ def quote():
         quote = lookup(symbol.upper())
         if not quote:
             return apology("Invalid Stock")
-        return render_template("quoted.html", name = quote["name"], price = quote["price"], symbol = quote["symbol"])
+        return render_template("quoted.html", quote=quote)
 
 
 @app.route("/register", methods=["GET", "POST"])
