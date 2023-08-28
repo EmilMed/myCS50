4bb4a4a00ec85b9cf4ea7fc86498ea6c0d23b7ed
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-28 08:58:21 +0000

    Mon, Aug 28, 2023, 9:58 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -41,7 +41,7 @@ def index():
     for stock in cashflow_db:
         quote = lookup(stock["symbol"])
         stock["name"] = quote["name"]
-        stcok["price"] = quote["price"]
+        stock["price"] = quote["price"]
     return render_template("index.html", cashflow_db=cashflow_db, cash = cash)
 
 @app.route("/buy", methods=["GET", "POST"])
