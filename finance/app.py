309334d1d27c92a702657046ca44d607766bb563
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-29 09:55:07 +0000

    Tue, Aug 29, 2023, 10:55 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -38,14 +38,12 @@ def index():
     stocks = db.execute("SELECT symbol, SUM(shares) AS total_shares, price FROM cashflow WHERE user_id = ? GROUP BY symbol HAVING total_shares > 0", user_id)
     cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
     cash = usd(cash_db[0]["cash"])
-    total_value = cash
     for stock in stocks:
         quote = lookup(stock["symbol"])
         stock["name"] = quote["name"]
         stock["price"] = usd(quote["price"])
-        stock["value"] = stock["price"] * stock["total_shares"]
-        total_value += stock["value"]
-    return render_template("index.html", stocks=stocks, cash=cash, total_value=total_value)
+        stock["value"] = usd(int(quote["price"]) * stock["total_shares"])
+    return render_template("index.html", stocks=stocks, cash=cash)
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
