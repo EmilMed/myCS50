4a4448dc5166801c2ae9fe5aed5cc6f347a3597b
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-31 11:18:07 +0000

    Thu, Aug 31, 2023, 12:18 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -32,21 +32,25 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    user_id = session["user_id"]
-    stocks = db.execute("SELECT symbol, SUM(shares) AS total_shares, price FROM cashflow WHERE user_id = ? GROUP BY symbol HAVING total_shares > 0", user_id)
-    cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
-    cash = cash_db[0]["cash"]
-    total_value = cash
-    grand_total = cash
-    for stock in stocks:
-        quote = lookup(stock["symbol"])
-        stock["name"] = quote["name"]
-        stock["price"] = quote["price"]
-        stock["value"] = stock["price"] * stock["total_shares"]
-        total_value += stock["value"]
-        grand_total += stock["value"]
-        stock["valueusd"] = usd(stock["value"])
-    return render_template("index.html", stocks=stocks, cash=cash, total=usd(total_value), grand=usd(grand_total))
+    """Show portfolio of stocks"""
+    # Create a table for index
+    rows = db.execute("SELECT symbol, SUM(shares) FROM transactions WHERE user_id=:user_id GROUP BY symbol HAVING SUM(shares) > 0", user_id=session["user_id"])
+
+    # Creates a place to save the informations
+    holdings = []
+    all_total = 0
+
+    for row in rows:
+        stock = lookup(row['symbol'])
+        sum_value = (stock["price"] * row["SUM(shares)"])
+        holdings.append({"symbol": stock["symbol"], "name": stock["name"], "shares": row["SUM(shares)"], "price": usd(stock["price"]), "total": usd(sum_value)})
+        all_total += stock["price"] * row["SUM(shares)"]
+
+    rows = db.execute("SELECT cash FROM users WHERE id=:user_id", user_id=session["user_id"])
+    cash = rows[0]["cash"]
+    all_total += cash
+
+    return render_template("index.html", holdings=holdings, cash=usd(cash), all_total=usd(all_total))
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
