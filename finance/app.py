a56f92466872499a08ad9a4d7ca4f216bee5aaf2
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-08-31 10:50:32 +0000

    Thu, Aug 31, 2023, 11:50 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -46,7 +46,7 @@ def index():
         total_value += stock["value"]
         grand_total += stock["value"]
         stock["valueusd"] = usd(stock["value"])
-    return render_template("index.html", stocks=stocks, cash=cash, total_value=total_value, grand_total=grand_total)
+    return render_template("index.html", stocks=stocks, cash=cash, total=usd(total_value), grand=usd(grand_total))
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
