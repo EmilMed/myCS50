b522fcdb81a7fb10a818f8c5b15f69a7f2107ede
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-09-12 22:15:45 +0000

    Tue, Sep 12, 2023, 11:15 PM +01:00

--- project/mp.py
+++ project/mp.py
@@ -7,3 +7,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 from helpers import apology, login_required, lookup, usd
 
+app = Flask(__name__)
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
