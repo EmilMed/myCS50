c38132976d9b57ec3ef352a646edb1d761ac8afd
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-09-17 10:05:17 +0000

    Sun, Sep 17, 2023, 11:05 AM +01:00

--- finance/app.py
+++ finance/app.py
@@ -73,13 +73,6 @@ def buy():
         flash("Successfully purchased!")
         return render_template("/bought.html", name=quote["name"], price=usd(quote["price"]), symbol=quote["symbol"])
 
-@app.route("/history")
-@login_required
-def history():
-    user_id = session["user_id"]
-    cashflow_db = db.execute("SELECT * FROM cashflow WHERE user_id =:id", id=user_id)
-    return render_template("history.html", cashflow = cashflow_db)
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -116,7 +109,6 @@ def login():
     else:
         return render_template("login.html")
 
-
 @app.route("/logout")
 def logout():
     """Log user out"""
@@ -126,20 +118,6 @@ def logout():
 
     # Redirect user to login form
     return redirect("/")
-
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    if request.method == "GET":
-        return render_template("quote.html")
-    else:
-        symbol = request.form.get("symbol")
-        quote = lookup(symbol.upper())
-        if not quote:
-            return apology("Invalid Stock")
-        return render_template("quoted.html", name=quote["name"], price=usd(quote["price"]), symbol=quote["symbol"])
-
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -159,41 +137,6 @@ def register():
         except:
             return apology("Already in use")
         session["user_id"] = user1
-        return redirect("/")
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    if request.method == "GET":
-        user_id = session["user_id"]
-        symbols_user = db.execute("SELECT symbol FROM cashflow WHERE user_id = :id GROUP BY symbol HAVING SUM(shares) > 0", id=user_id)
-        return render_template("sell.html", symbols = [row["symbol"] for row in symbols_user])
-    else:
-        shares = int(request.form.get("shares"))
-        symbol = request.form.get("symbol")
-        if not shares:
-            return apology("Must input number of shares")
-        quote = lookup(symbol.upper())
-        if quote == None:
-            return apology("Invalid Stock")
-        if shares < 0:
-            return apology("Shares has to be a positive number!")
-
-        total_cost = shares * quote["price"]
-        user_id = session["user_id"]
-        cash_atm_db = db.execute("SELECT cash FROM users WHERE id = :id", id=user_id)
-        user_cash = cash_atm_db[0]["cash"]
-
-        total_shares = db.execute("SELECT SUM(shares) as shares from cashflow WHERE user_id=:id AND symbol = :symbol GROUP BY symbol", id=user_id, symbol=symbol)
-        total_shares_atm = total_shares[0]["shares"]
-        if shares > total_shares_atm:
-            return apology("You don't own that many shares")
-
-        new_cash = user_cash + total_cost
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", new_cash, user_id)
-        date = datetime.datetime.now()
-        db.execute("INSERT INTO cashflow (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", user_id, quote["symbol"], (-1) * shares, quote["price"], date)
-        flash("Successfully sold!")
         return redirect("/")
 
 @app.route("/top_up_balance", methods=["GET", "POST"])
