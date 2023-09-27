890e5f7a76c81a7ffab905753027b27526a0a659
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-09-27 21:05:09 +0000

    Wed, Sep 27, 2023, 10:05 PM +01:00

--- project/app.py
+++ project/app.py
@@ -19,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///project.db")
+db = SQL("sqlite:///finance.db")
 
 
 @app.after_request
