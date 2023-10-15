f9e9edb64ed8d5e04f63e34558b0e31d1cd1a6d3
EmilMed <107815859+EmilMed@users.noreply.github.com>
2023-10-15 19:17:18 +0000

    Sun, Oct 15, 2023, 8:17 PM +01:00

--- finance/app.py
+++ finance/app.py
@@ -19,7 +19,6 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
 
 @app.after_request
 def after_request(response):
